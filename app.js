8cd3400a67fba5e8208c5abcd0ab59f7a273cf29
RagePeanut <dorian.gruselin@hotmail.com>
2018-04-26 01:20:17 +0200

    Added support for posts tweeting

--- app.js
+++ app.js
@@ -31,87 +31,15 @@ function stream() {
             // Errors are mostly caused by RPC nodes crashing
             if(err) return reject(err);
             // Resteems are inside custom_json operations
-            if(operation[0] === 'custom_json') {
+            if(settings.tweet_resteems && operation[0] === 'custom_json') {
                 const op = JSON.parse(operation[1].json);
-                // Checking if it's a resteem and if the user wants to tweet resteems from the specified account
+                // Checking if it's a resteem and if it's from one of the specified accounts
                 if(op[0] === 'reblog' && steem_accounts.includes(op[1].account)) {
-                    // Getting the content of the post
-                    steem.api.getContent(op[1].author, op[1].permlink, (err, result) => {
-                        if(err) console.log(err.message, 'encountered while getting the content of /@' + op[1].author + '/' + op[1].permlink);
-                        else {
-                            const metadata = JSON.parse(result.json_metadata);
-                            let message = '';
-                            // Title
-                            if(settings.include_title) {
-                                // Mentions
-                                // If set to true, completely removes the mentions in the title (e.g. 'Hello @ragepeanut !' --> 'Hello !')
-                                if(settings.remove_mentions) result.title = result.title.replace(/( )?@[a-zA-Z0-9._-]+( )?/g, (match, firstSpace, secondSpace) => { return firstSpace || secondSpace});
-                                // If set to true, removes the @ character from mentions (e.g. 'Bye @ragepeanut !' --> 'Bye ragepeanut !')
-                                else if(settings.remove_mentions_at_char) result.title = result.title.replace(/@([a-zA-Z0-9._-]+)/g, '$1');
-                                // If set to true, escapes a mention if it is the first word of the title (e.g. '@ragepeanut isn\'t a real peanut :O' --> '.@ragepeanut isn\'t an real peanut :O')
-                                else if(settings.escape_starting_mention && result.title[0] === '@') result.title = '.' + result.title;
-                                message += result.title + ' ';
-                            }
-                            // Tags
-                            if(settings.include_tags) {
-                                let tags = metadata.tags || [result.category];
-                                // If set to true, removes any duplicate tag from the tags array
-                                if(settings.check_for_duplicate_tags) {
-                                    const tmpTags = [];
-                                    tags.forEach(tag => {
-                                        if(!tmpTags.includes(tag)) tmpTags.push(tag);
-                                    });
-                                    tags = tmpTags;
-                                }
-                                // If set to an integer, takes only the X first tags
-                                if(settings.tag_limit) tags = tags.slice(0, settings.tag_limit);
-                                // Unshifting to put a # character in front of the first tag
-                                tags.unshift('');
-                                message += tags.reduce((accumulator, tag) => accumulator + '#' + tag + ' ');
-                                // Checking if the message is going to be longer than the maximum length
-                                if(message.length + LINK_LENGTH > MAX_TWEET_LENGTH) {
-                                    tags.shift();
-                                    let neededLength = message.length + LINK_LENGTH - MAX_TWEET_LENGTH;
-                                    // If set to true, removes tags by order of importance (last tag removed first)
-                                    if(settings.remove_tags_by_order) {
-                                        for(let i = tags.length - 1; i >= 0 && neededLength > 0; i--) {
-                                            message = message.replace('#' + tags[i] + ' ', '');
-                                            neededLength -= tags[i].length + 2;
-                                        }
-                                    // If set to true, removes tags by the opposite order of importance (first tag removed first)
-                                    } else if(settings.remove_tags_by_order_opposite) {
-                                        for(let i = 0; i < tags.length && neededLength > 0; i++) {
-                                            message = message.replace('#' + tags[i] + ' ', '');
-                                            neededLength -= tags[i].length + 2;
-                                        }
-                                    // If set to true, removes tags by length (smallest removed first)
-                                    } else if(settings.remove_tags_by_length) {
-                                        tags = tags.sort((a, b) => a.length - b.length);
-                                        for(let i = 0; i < tags.length && neededLength > 0; i++) {
-                                            message = message.replace('#' + tags[i] + ' ', '');
-                                            neededLength -= tags[i].length + 2;
-                                        }
-                                    // If set to true, removes tags by length (longest removed first)
-                                    } else if(settings.remove_tags_by_length_opposite) {
-                                        tags = tags.sort((a, b) => b.length - a.length);
-                                        for(let i = 0; i < tags.length && neededLength > 0; i++) {
-                                            message = message.replace('#' + tags[i] + ' ', '');
-                                            neededLength -= tags[i].length + 2;
-                                        }
-                                    }
-                                }
-                            }
-                            // If the message is too long, trims the title
-                            if(message.length + LINK_LENGTH > MAX_TWEET_LENGTH) {
-                                let neededLength = message.length + LINK_LENGTH - MAX_TWEET_LENGTH;
-                                message = message.replace(result.title, result.title.substr(0, result.title.length - neededLength - 3) + '...');
-                            }
-                            // First parameter (app): checking for all the known ways of specifying an app, if none of them exists the app is set to undefined
-                            message += getWebsite(metadata.community || (metadata.app && (metadata.app.name || metadata.app.split('/')[0])) || undefined, result.author, result.permlink, result.url, metadata.tags, result.body)
-                            tweet(message);
-                        }
-                    });
+                    treatOperation(op[1].account, op[1].permlink);
                 }
+            // Checking if it's a post (not a comment) made by one of the specified accounts
+            } else if(settings.tweet_posts && operation[0] === 'comment' && steem_accounts.includes(operation[1].author) && operation[1].parent_author === '') {
+                treatOperation(operation[1].author, operation[1].permlink);
             }
         });
     // If an error occured, add 1 to the index and put it at 0 if it is out of bound
@@ -133,7 +61,7 @@ function tweet(message) {
 // Gets the website from which the post has been made if it can view posts
 function getWebsite(app, author, permlink, url, tags, body) {
     // If the app is steemit or linking to posts on the app is not allowed/supported, the post is treated as a steemit post
-    // Apps that get a steemit.com link: steemit, chainbb, esteem, masdacs, steemauto, steempress, Steem Harry Games, vote-buyer, steemjs, piston-lib, undefined
+    // Apps that get a steemit.com link: steemit, chainbb, esteem, masdacs, steemauto, steempress, postpromoter, Steem Harry Games, vote-buyer, steemjs, piston-lib, undefined
     // The list of supported apps is manually updated. If an app is missing, please contact me through any of the means specified in the README file or send a new issue
     if(!settings.allowed_links[app]) return 'steemit.com' + url;
     switch(app) {
@@ -172,4 +100,83 @@ function getWebsite(app, author, permlink, url, tags, body) {
         default:
             return 'steemit.com' + url;
     }
+}
+
+function treatOperation(author, permlink) {
+    // Getting the content of the post
+    steem.api.getContent(author, permlink, (err, result) => {
+        if(err) console.log(err.message, 'encountered while getting the content of /@' + author + '/' + permlink);
+        else {
+            const metadata = JSON.parse(result.json_metadata);
+            let message = '';
+            // Title
+            if(settings.include_title) {
+                // Mentions
+                // If set to true, completely removes the mentions in the title (e.g. 'Hello @ragepeanut !' --> 'Hello !')
+                if(settings.remove_mentions) result.title = result.title.replace(/( )?@[a-zA-Z0-9._-]+( )?/g, (match, firstSpace, secondSpace) => { return firstSpace || secondSpace});
+                // If set to true, removes the @ character from mentions (e.g. 'Bye @ragepeanut !' --> 'Bye ragepeanut !')
+                else if(settings.remove_mentions_at_char) result.title = result.title.replace(/@([a-zA-Z0-9._-]+)/g, '$1');
+                // If set to true, escapes a mention if it is the first word of the title (e.g. '@ragepeanut isn\'t a real peanut :O' --> '.@ragepeanut isn\'t an real peanut :O')
+                else if(settings.escape_starting_mention && result.title[0] === '@') result.title = '.' + result.title;
+                message += result.title + ' ';
+            }
+            // Tags
+            if(settings.include_tags) {
+                let tags = metadata.tags || [result.category];
+                // If set to true, removes any duplicate tag from the tags array
+                if(settings.check_for_duplicate_tags) {
+                    const tmpTags = [];
+                    tags.forEach(tag => {
+                        if(!tmpTags.includes(tag)) tmpTags.push(tag);
+                    });
+                    tags = tmpTags;
+                }
+                // If set to an integer, takes only the X first tags
+                if(settings.tag_limit) tags = tags.slice(0, settings.tag_limit);
+                // Unshifting to put a # character in front of the first tag
+                tags.unshift('');
+                message += tags.reduce((accumulator, tag) => accumulator + '#' + tag + ' ');
+                // Checking if the message is going to be longer than the maximum length
+                if(message.length + LINK_LENGTH > MAX_TWEET_LENGTH) {
+                    tags.shift();
+                    let neededLength = message.length + LINK_LENGTH - MAX_TWEET_LENGTH;
+                    // If set to true, removes tags by order of importance (last tag removed first)
+                    if(settings.remove_tags_by_order) {
+                        for(let i = tags.length - 1; i >= 0 && neededLength > 0; i--) {
+                            message = message.replace('#' + tags[i] + ' ', '');
+                            neededLength -= tags[i].length + 2;
+                        }
+                    // If set to true, removes tags by the opposite order of importance (first tag removed first)
+                    } else if(settings.remove_tags_by_order_opposite) {
+                        for(let i = 0; i < tags.length && neededLength > 0; i++) {
+                            message = message.replace('#' + tags[i] + ' ', '');
+                            neededLength -= tags[i].length + 2;
+                        }
+                    // If set to true, removes tags by length (smallest removed first)
+                    } else if(settings.remove_tags_by_length) {
+                        tags = tags.sort((a, b) => a.length - b.length);
+                        for(let i = 0; i < tags.length && neededLength > 0; i++) {
+                            message = message.replace('#' + tags[i] + ' ', '');
+                            neededLength -= tags[i].length + 2;
+                        }
+                    // If set to true, removes tags by length (longest removed first)
+                    } else if(settings.remove_tags_by_length_opposite) {
+                        tags = tags.sort((a, b) => b.length - a.length);
+                        for(let i = 0; i < tags.length && neededLength > 0; i++) {
+                            message = message.replace('#' + tags[i] + ' ', '');
+                            neededLength -= tags[i].length + 2;
+                        }
+                    }
+                }
+            }
+            // If the message is too long, trims the title
+            if(message.length + LINK_LENGTH > MAX_TWEET_LENGTH) {
+                let neededLength = message.length + LINK_LENGTH - MAX_TWEET_LENGTH;
+                message = message.replace(result.title, result.title.substr(0, result.title.length - neededLength - 3) + '...');
+            }
+            // First parameter (app): checking for all the known ways of specifying an app, if none of them exists the app is set to undefined
+            message += getWebsite(metadata.community || (metadata.app && (metadata.app.name || metadata.app.split('/')[0])) || undefined, result.author, result.permlink, result.url, metadata.tags, result.body)
+            tweet(message);
+        }
+    });
 }
