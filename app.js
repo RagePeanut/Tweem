6ea901a23ccd050469c4049fbc150384ee17261f
RagePeanut <dorian.gruselin@hotmail.com>
2018-09-22 14:52:37 +0200

    Added image support for crossposting

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const Twit = require('twit');
 const steemStream = require('steem');
 const steemRequest = require('steem');
+const request = require('request').defaults({ encoding: 'base64' });
 
 const parser = require('./utils/parser');
 
-const { personal_steem_accounts, request_nodes, settings, steem_accounts, stream_nodes, template, tweet_retry_timeout, twitter_handle } = require('./config');
+const { request_nodes, settings, steem_accounts, stream_nodes, template, tweet_retry_timeout, twitter_handle } = require('./config');
 
 const twitter = new Twit({
     consumer_key: process.env.CONSUMER_KEY,
@@ -17,17 +18,12 @@ const twitter = new Twit({
 const MAX_TWEET_LENGTH = 280;
 // Twitter automatically replaces links by https://t.co/[A-Za-z\d]{10}
 const LINK_LENGTH = 23;
+// The current maximum amount of images linked to a tweet is 4
+const MAX_IMAGE_COUNT = 4;
 
 // Sending tweets with a specified delay between them
 const tweetStack = [];
-let tweetsInterval = setInterval(() => {
-    if(tweetStack[0]) {
-        // Making sure that no tweet is sent while processing this one
-        clearInterval(tweetsInterval);
-        const [message, url] = tweetStack.shift();
-        tweet(message, url);
-    }
-}, settings.tweet_frequency_minutes * 60 * 1000);
+let tweetsInterval = setInterval(processTweet, settings.tweet_frequency_minutes * 60 * 1000);
 
 steemRequest.api.setOptions({ url: request_nodes[0] });
 
@@ -66,36 +62,29 @@ function stream() {
 }
 
 /**
- * Tweets a message if the related URL hasn't been tweeted yet
- * @param {string} message The tweet content
- * @param {string} url The post's URL
+ * Tweets if the related URL hasn't been tweeted yet
+ * @param {string} params The tweet content
+ * @param {string} [url] The post's URL
  */
-function tweet(message, url) {
+function tweet(params, url) {
     // Checking if the link has already been included in a tweet from this account
     isAlreadyTweeted(url)
         .then(alreadyTweeted => {
             if(!alreadyTweeted) {
-                twitter.post('statuses/update', { status: message }, (err, data, response) => {
+                twitter.post('statuses/update', params, err => {
                     if(err) {
                         // The tweet already exists
-                        if(err.code === 187) console.error('Error: The tweet \'' + message + '\' already exists');
+                        if(err.code === 187) console.error('Error: The tweet \'' + params.status + '\' already exists');
                         // Retry if it's another error
                         else {
                             console.error('Unknown error:', err.message);
                             console.log('Please notify @ragepeanut of the encountered error so it doesn\'t happen to other users')
                             console.log('Retrying...');
-                            setTimeout(tweet, tweet_retry_timeout, message, url);
+                            setTimeout(tweet, tweet_retry_timeout, params, url);
                         }
                     } else {
-                        console.log('Successfully tweeted', message);
-                        tweetsInterval = setInterval(() => {
-                            if(tweetStack[0]) {
-                                // Making sure that no tweet is sent while processing this one
-                                clearInterval(tweetsInterval);
-                                const [message, url] = tweetStack.shift();
-                                tweet(message, url);
-                            }
-                        }, settings.tweet_frequency_minutes * 60 * 1000);
+                        console.log('Successfully tweeted', params.status);
+                        tweetsInterval = setInterval(processTweet, settings.tweet_frequency_minutes * 60 * 1000);
                     }
                 });
             } else console.log('A tweet with the same link has already been sent.');
@@ -103,22 +92,57 @@ function tweet(message, url) {
         .catch(err => {
             console.error('Twitter Search API Error:', err.message);
             console.log('Retrying...');
-            setTimeout(tweet, tweet_retry_timeout, message, url);
+            setTimeout(tweet, tweet_retry_timeout, params, url);
         });
+}
+
+/** Processes a tweet */
+function processTweet() {
+    if(tweetStack[0]) {
+        // Making sure that no tweet is sent while processing this one
+        clearInterval(tweetsInterval);
+        const [params, url] = tweetStack.shift();
+        if(url) tweet(params, url);
+        else {
+            Promise.all(params.media_ids.slice(0, MAX_IMAGE_COUNT).map(url => uploadImage(url)))
+                   .then(media_ids => {
+                       params.media_ids = media_ids.filter(id => id !== 'NOT_UPLOADED');
+                       tweet(params);
+                   });
+        }
+    }
+}
+
+/** 
+ * Uploads an image on Twitter
+ * @param {string} url The image's url
+ * @returns {Promise} Unrejectable promise resolving to the uploaded media ID or to 'NOT_UPLOADED' 
+ */
+function uploadImage(url) {
+    return new Promise(resolve => {
+        request.get(url, (error, response, body) => {
+            if(!error && response.statusCode === 200) {
+                twitter.post('media/upload', { media_data: body }, (error, data) => {
+                    if(error) resolve('NOT_UPLOADED');
+                    else resolve(data.media_id_string);
+                }); 
+            } else resolve('NOT_UPLOADED');
+        });
+    });
 }
 
 /**
  * Checks if a link has already been tweeted in the last 7 days
  * This function is still a prototype and may resolve to false with some already tweeted links
- * @param {string} url The post's url
- * @returns {Promise} Unrejectable promise resolving to true if a URL has already been tweeted and false otherwise
+ * @param {string} [url] The post's url
+ * @returns Unrejectable promise resolving to the uploaded media id Unrejectable promise resolving to true if a URL has already been tweeted and false otherwise
  */
 function isAlreadyTweeted(url) {
-    // The Twitter Search API is a mess, can't handle the @ character
-    const urlAllowedParts = url.split('@');
     return new Promise(resolve => {
-        // Tweet-like post
-        if(url === '') resolve(false);
+        // Tweet-like posts
+        if(!url) resolve(false);
+        // The Twitter Search API is a mess, can't handle the @ character
+        const urlAllowedParts = url.split('@');
         const query = { q: 'url:' + urlAllowedParts[urlAllowedParts.length - 1], count: 100, result_type: 'recent' };
         twitter.get('search/tweets', query, (err, data, response) => {
             if(err) resolve(false);
@@ -268,7 +292,8 @@ function processOperation(author, permlink, type) {
                         tweetStructure = removeLeastImportant(tweetStructure);
                     }
                     tweetStructure.parsed = tweetStructure.parsed.replace(values.link, website);
-                    tweetStack.push([tweetStructure.parsed, website]);
+                    if(templateType === 'tweet_like') tweetStack.push([{ status: tweetStructure.parsed, media_ids: metadata.image || [] }]);
+                    else tweetStack.push([{ status: tweetStructure.parsed }, website]);
                 }
             }
         });
